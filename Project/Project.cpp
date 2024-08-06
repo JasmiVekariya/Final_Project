ccc2ac6b3e9aefa4e9cccd9fbd6937db30fbef73
Jasmi Vekariya <jasmivekariya1@gmail.com>
2024-08-06 12:02:00 -0400

    Addition of header file to the program.

---
+++ Project/Project.cpp
@@ -0,0 +1,8 @@
+/*
+FILE          : project.cpp
+PROJECT       : Final Project
+PROGRAMMER    : Helly Shah (8958841)
+                Jasmi Devjibhai Vekariya (8960251)
+FIRST VERSION : August 6, 2024
+DESCRIPTION   : The below program delineates the code which calculates the different statistics about the items to be delivered through courier company.
+*/
