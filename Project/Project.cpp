1ef6db772bb79ff634bb3d82070c9f075ea7b73a
Helly Shah <shahhelly2193@gmail.com>
2024-08-06 12:32:23 -0400

    definition of struct Parcel

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -6,3 +6,23 @@ PROGRAMMER    : Helly Shah (8958841)
 FIRST VERSION : August 6, 2024
 DESCRIPTION   : The below program delineates the code which calculates the different statistics about the items to be delivered through courier company.
 */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define HASH_TABLE_SIZE 127
+
+struct Parcel 
+{
+    char* destination;
+    int weight;
+    float value;
+    Parcel* left;
+    Parcel* right;
+};
+
+struct HashTableEntry 
+{
+    Parcel* root;
+};
